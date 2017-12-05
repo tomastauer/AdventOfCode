286447dac3d74b6d1664f6b41c75031b7645c9c2
Tomas Tauer <totauer@microsoft.com>
2017-12-05 09:47:02 +0100

    Day2

---
+++ AdventOfCode/Solutions/Day2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Solutions
+{
+    using System.Runtime.Remoting.Messaging;
+
+    internal class Day2 : Solution
+    {
+        protected override string RunInternalPart1(string input)
+        {
+            return input.Split(new[] { Environment.NewLine },StringSplitOptions.None).Select(line =>
+            {
+                var numbers = line.Split('\t').Select(int.Parse).ToList();
+                return numbers.Max() - numbers.Min();
+            }).Sum().ToString();
+        }
+
+        protected override string RunInternalPart2(string input)
+        {
+            return input.Split(new[] { Environment.NewLine }, StringSplitOptions.None).Select(line =>
+            {
+                var numbers = line.Split('\t').Select(int.Parse).ToList();
+                return numbers.Select(a =>
+                {
+                    var c = numbers.SingleOrDefault(b => a % b == 0 && a != b);
+                    return c == 0 ? 0 : a / c;
+                }).Max();
+            }).Sum().ToString();
+        }
+    }
+}
