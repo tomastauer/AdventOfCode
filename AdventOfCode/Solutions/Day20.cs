89d981f8e8a9ec45e366ba68d9fccb202ece7920
Tomas Tauer <totauer@microsoft.com>
2017-12-21 09:13:36 +0100

    Day 21

---
+++ AdventOfCode/Solutions/Day20.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Solutions
+{
+    internal class Day20 : Solution
+    {
+        protected override string RunInternalPart1(string input)
+        {
+            var particles = input.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).Select(c => new Particle(c)).ToList();
+            for(int i = 0; i < 5000; i++)
+            {
+                particles.ForEach(c => c.Evolve());
+            }
+
+            var evolved = new List<Particle>(particles);
+            while(evolved.Any(c=>c.IsGettingCloser()))
+            {
+                evolved.ForEach(c => c.Evolve());
+            }
+
+            return evolved.IndexOf(evolved.Single(c=>c.Increment() == evolved.Min(d=>d.Increment()))).ToString();
+        }
+
+        protected override string RunInternalPart2(string input)
+        {
+            var particles = input.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).Select(c => new Particle(c)).ToList();
+            var particlesSet = new HashSet<Particle>(particles);
+            for (int i = 0; i < 5000; i++)
+            {
+                particles.ForEach(c => c.Evolve());
+                particles = particles.GroupBy(c => c.Position).Where(c => c.Count() == 1).Select(c => c.Single()).ToList();
+            }
+
+            return particles.Count.ToString();
+        }
+
+        private class Particle
+        {
+            private const string pattern = @"p=<(?<px>-?\d+),(?<py>-?\d+),(?<pz>-?\d+)>, v=<(?<vx>-?\d+),(?<vy>-?\d+),(?<vz>-?\d+)>, a=<(?<ax>-?\d+),(?<ay>-?\d+),(?<az>-?\d+)>";
+
+            public Particle(string line)
+            {
+                var match = Regex.Match(line, pattern);
+                Position = (int.Parse(match.Groups["px"].Value), int.Parse(match.Groups["py"].Value), int.Parse(match.Groups["pz"].Value));
+                Velocity = (int.Parse(match.Groups["vx"].Value), int.Parse(match.Groups["vy"].Value), int.Parse(match.Groups["vz"].Value));
+                Acceleration = (int.Parse(match.Groups["ax"].Value), int.Parse(match.Groups["ay"].Value), int.Parse(match.Groups["az"].Value));
+
+                Distances = new List<int>();
+            }
+            
+            public (int, int, int) Position { get; set; }
+            public (int, int, int) Velocity { get; set; }
+            public (int, int, int) Acceleration { get; set; }
+
+            private List<int> Distances { get; set; }
+
+            public void Evolve()
+            {
+                Distances.Add(GetDistance());
+                Velocity = (Velocity.Item1 + Acceleration.Item1, Velocity.Item2 + Acceleration.Item2, Velocity.Item3 + Acceleration.Item3);
+                Position = (Velocity.Item1 + Position.Item1, Velocity.Item2 + Position.Item2, Velocity.Item3 + Position.Item3);
+            }
+
+            public int GetDistance()
+            {
+                return Math.Abs(Position.Item1) + Math.Abs(Position.Item2) + Math.Abs(Position.Item3);
+            }
+
+            public bool IsGettingCloser()
+            {
+                return Distances.Last() == Distances.Min() && Distances.Last() != Distances.First();
+            }
+
+            public int Increment()
+            {
+                return Math.Abs(Distances[Distances.Count - 1] - Distances[Distances.Count - 2]);
+            }
+        }
+    }
+}
---
+++ AdventOfCode/Solutions/Day21.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Solutions
+{
+    internal class Day21 : Solution
+    {
+        protected override string RunInternalPart1(string input)
+        {
+            var transformations = input.Split(new[] {Environment.NewLine}, StringSplitOptions.None)
+                .Select(c => new Transformation(c)).ToList();
+
+            var fragments = GetFragments(Pixelize(".#./..#/###"));
+
+            for (int i = 0; i < 5; i++)
+            {
+                fragments = GetFragments(Pixelize(EvolveFragments(fragments, transformations)));
+            }
+
+            return Depixelize(Pixelize(fragments)).Count(c => c == '#').ToString();
+        }
+
+        protected override string RunInternalPart2(string input)
+        {
+            var transformations = input.Split(new[] { Environment.NewLine }, StringSplitOptions.None)
+                .Select(c => new Transformation(c)).ToList();
+
+            var fragments = GetFragments(Pixelize(".#./..#/###"));
+
+            for (int i = 0; i < 18; i++)
+            {
+                fragments = GetFragments(Pixelize(EvolveFragments(fragments, transformations)));
+            }
+
+            return Depixelize(Pixelize(fragments)).Count(c => c == '#').ToString();
+        }
+
+        private char[][] Pixelize(string pattern)
+        {
+            return pattern.Split('/').Select(c => c.ToArray()).ToArray();
+        }
+
+        private char[][] Pixelize(Fragment[,] fragments)
+        {
+            var fragmentsSize = fragments.GetLength(0);
+            var fragmentSize = fragments[0, 0].Size;
+            var outputSize = fragments[0, 0].Size * fragmentsSize;
+            var result = new char[outputSize][];
+            for (int i = 0; i < outputSize; i++)
+            {
+                result[i] = new char[outputSize];
+            }
+
+            for (int i = 0; i < fragmentsSize; i++)
+            {
+                for (int j = 0; j < fragmentsSize; j++)
+                {
+                    for (int x = 0; x < fragmentSize; x++)
+                    {
+                        for (int y = 0; y < fragmentSize; y++)
+                        {
+                            result[i*fragmentSize + x][j*fragmentSize + y] = fragments[i,j].Pixels[x][y];
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private string Depixelize(char[][] pixels)
+        {
+            return string.Join("/", pixels.Select(c => new string(c)).ToArray());
+        }
+
+        private Fragment[,] EvolveFragments(Fragment[,] fragments, List<Transformation> transformations)
+        {
+            int size = fragments.GetLength(0);
+
+            var result = new Fragment[size, size];
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    result[i,j] = new Fragment(transformations.Single(c => c.MatchInput(Depixelize(fragments[i, j].Pixels))).GetOutput());
+                }
+            }
+
+            return result;
+        }
+        
+        private Fragment[,] GetFragments(char[][] pixels)
+        {
+            int denominator = pixels.Length % 2 == 0 ? 2 : 3;
+            var resultSize = pixels.Length/denominator;
+            var result = new Fragment[resultSize,resultSize];
+
+            for (int i = 0; i < resultSize; i++)
+            {
+                for (int j = 0; j < resultSize; j++)
+                {
+                    var part = new char[denominator][];
+
+                    for (int x = 0; x < denominator; x++)
+                    {
+                        part[x] = new char[denominator];
+
+                        for (int y = 0; y < denominator; y++)
+                        {
+                            part[x][y] = pixels[i * denominator + x][j * denominator + y];
+                        }
+                    }
+
+                    result[i, j] = new Fragment(part);
+                }
+            }
+            return result;
+        }
+
+        private class Fragment
+        {
+            public char[][] Pixels { get; }
+
+            public int Size => Pixels.Length;
+
+            public Fragment(char[][] pixels)
+            {
+                Pixels = pixels;
+            }
+        }
+
+        private class Transformation
+        {
+            public Transformation(string transformation)
+            {
+                var parts = transformation.Split(new[] {" => "}, StringSplitOptions.None);
+                variations = GetAllVariations(parts[0]);
+                output = parts[1];
+            }
+
+            private readonly List<string> variations;
+            private readonly string output;
+
+            public bool MatchInput(string pattern)
+            {
+                return variations.Contains(pattern);
+            }
+
+            public char[][] GetOutput()
+            {
+                return Pixelize(output);
+            }
+
+            private List<string> GetAllVariations(string pattern)
+            {
+                var result = new List<string>();
+                var pixels = Pixelize(pattern);
+
+                for (int i = 0; i < 4; i++)
+                {
+                    pixels = Rotate(pixels);
+                    result.Add(Depixelize(pixels));
+                    result.Add(Depixelize(Flip(pixels)));
+                }
+
+                return result;
+            }
+
+            private char[][] Pixelize(string pattern)
+            {
+                return pattern.Split('/').Select(c => c.ToArray()).ToArray();
+            }
+
+            private string Depixelize(char[][] pixels)
+            {
+                return string.Join("/", pixels.Select(c => new string(c)).ToArray());
+            }
+
+            private char[][] Rotate(char[][] pixels)
+            {
+                var result = new char[pixels.Length][];
+
+                for (int i = 0; i < pixels.Length; i++)
+                {
+                    result[i] = new char[pixels.Length];
+
+                    for (int j = 0; j < pixels.Length; j++)
+                    {
+                        result[i][j] = pixels[j][pixels.Length - i - 1];
+                    }
+                }
+
+                return result;
+            }
+
+            private char[][] Flip(char[][] pixels)
+            {
+                var result = new char[pixels.Length][];
+
+                for (int i = 0; i < pixels.Length; i++)
+                {
+                    result[i] = new char[pixels.Length];
+
+                    for (int j = 0; j < pixels.Length; j++)
+                    {
+                        result[i][j] = pixels[pixels.Length - i - 1][j];
+                    }
+                }
+
+                return result;
+            }
+        }
+    }
+}
